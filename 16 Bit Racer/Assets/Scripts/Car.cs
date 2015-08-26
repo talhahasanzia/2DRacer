144b50f21c9452cbc34c98a1d10e8e17f42d0fdc
Talha Hasan Zia <talhahasanzia@hotmail.com>
2015-08-26 09:32:21 +0500

    Basic Play

---
+++ 16 Bit Racer/Assets/Scripts/Car.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Car : MonoBehaviour {
+    Quaternion startRotation;
+    Quaternion constRotation;
+
+
+
+	// Use this for initialization
+	void Start () {
+        startRotation=transform.rotation;
+        constRotation=new Quaternion(startRotation.x,startRotation.y,startRotation.z,startRotation.w);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+        Vector3 newPositionForward=new Vector3(transform.position.x,transform.position.y+5,transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, newPositionForward, 2 * Time.deltaTime);
+        this.transform.rotation = constRotation;
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        { 
+        Vector3 newPosition=new Vector3(transform.position.x+2,transform.position.y,transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, newPosition,2*Time.deltaTime);
+        
+        
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Vector3 newPosition = new Vector3(transform.position.x - 2, transform.position.y, transform.position.z);
+            transform.position = Vector3.Lerp(transform.position, newPosition, 2 * Time.deltaTime);
+
+
+        }
+	}
+}
---
+++ 16 Bit Racer/Assets/Scripts/Spawning.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawning : MonoBehaviour {
+    
+    
+    public GameObject obj;
+    public float S_Max = 1f;
+    public float S_Min = 2f;
+    float incY = 9.36f;
+
+
+    public GameObject CarEnemy;
+
+
+
+    
+
+
+
+
+
+	// Use this for initialization
+	void Start () {
+        Spawn();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void Spawn()
+    {
+
+        Vector3 newPosition = new Vector3(obj.transform.position.x, obj.transform.position.y +incY , obj.transform.position.z);
+        Object newObject=Instantiate(obj,newPosition,Quaternion.identity);
+
+        //Destroy(newObject, 30);
+
+
+
+        float xPos = Random.Range(-1, 1);
+        xPos=Mathf.Ceil(xPos);
+
+        Debug.Log("X Position: " + xPos);
+
+        CarEnemy.transform.localPosition = new Vector3(xPos, CarEnemy.transform.localPosition.y, CarEnemy.transform.localPosition.z);
+        Invoke("Spawn",0.3f);
+        
+        incY += 9.36f;
+    
+    }
+}
